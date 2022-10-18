13bb7ca0c5a8c0c00a15102f26748cb0c7faedc3
weenawadee20 <116073435+weenawadee20@users.noreply.github.com>
2022-10-18 11:00:14 +0000

    เพิ่ม circle.py

---
+++ circle.py
@@ -0,0 +1,8 @@
+import turtle
+
+t = turtle.Turtle()
+
+for c in ['red', 'green', 'blue', 'yellow']:
+    t.color(c)
+    t.circle(75)
+    t.left(90)
--- main.py
+++ main.py
@@ -12,5 +12,5 @@ t = turtle.Turtle()
 
 for c in ['red', 'green', 'blue', 'yellow']:
     t.color(c)
-    t.forward(75)
+    t.circle(75)
     t.left(90)
